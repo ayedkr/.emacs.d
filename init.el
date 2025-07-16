aa0ce9f474c6170e2751e747e03d8fbdc2537979
Dylan <dylan@dylan-laptop>
2025-07-16 17:05:51 -0500

    stuff

--- init.el
+++ init.el
@@ -16,21 +16,11 @@
 
 (straight-use-package 'doom-themes)
 (straight-use-package 'doom-modeline)
-(doom-modeline-mode 1)
 (straight-use-package 'enlight)
 (straight-use-package 'magit)
 (straight-use-package 'tree-sitter)
 (straight-use-package 'tree-sitter-langs)
 (straight-use-package 'spacious-padding)
-
-(cond				       
- ((eq system-type 'gnu/linux) (add-to-list 'default-frame-alist '(font . "Terminus"))))
-
-
-(cond
- ((eq system-type 'windows-nt)
-  (add-to-list 'default-frame-alist '(font . "Terminus (TTF) for Windows"))
-  (setq default-directory "c:/Users/Dylan")))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -40,20 +30,19 @@
  '(column-number-mode nil)
  '(custom-enabled-themes '(doom-1337))
  '(custom-safe-themes
-   '("31014fae0ca149e8bbffe40826f8f5952fdb91ea534914622d614b2219e04eaf"
+   '("014cb63097fc7dbda3edf53eb09802237961cbb4c9e9abd705f23b86511b0a69"
+     "31014fae0ca149e8bbffe40826f8f5952fdb91ea534914622d614b2219e04eaf"
      default))
  '(display-line-numbers-type 'relative)
- '(doom-modeline-bar-width 4)
+ '(doom-modeline-bar-width 10)
  '(doom-modeline-buffer-name t)
  '(doom-modeline-check-icon nil)
- '(doom-modeline-height 36)
+ '(doom-modeline-height 64)
  '(doom-modeline-hud nil)
  '(doom-modeline-icon nil)
  '(doom-modeline-major-mode-icon nil)
  '(doom-modeline-minor-modes t)
  '(doom-modeline-mode t)
- '(doom-old-hope-padded-modeline nil)
- '(doom-themes-padded-modeline nil)
  '(enlight-content "EMACS")
  '(horizontal-scroll-bar-mode nil)
  '(initial-buffer-choice 'enlight)
@@ -61,20 +50,5 @@
  '(prog-mode-hook '(display-line-numbers-mode))
  '(scroll-bar-mode nil)
  '(spacious-padding-mode t)
- '(spacious-padding-widths
-   '(:internal-border-width 15 :header-line-width 4 :mode-line-width 0
-			    :tab-width 4 :right-divider-width 30
-			    :scroll-bar-width 8 :fringe-width 8))
- '(tab-bar-close-button-show nil)
- '(tab-bar-format
-   '(tab-bar-format-menu-bar tab-bar-format-history tab-bar-format-tabs
-			     tab-bar-separator))
- '(tab-bar-mode nil)
- '(tab-line-close-button-show nil)
  '(tool-bar-mode nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(custom-set-faces)
