067043630c651d07a0ec01bbf699590e2e42be76
dylan <dylank@posteo.com>
2025-08-02 21:05:05 -0500

    penis

--- init.el
+++ init.el
@@ -14,13 +14,29 @@
       (eval-print-last-sexp)))
   (load bootstrap-file nil 'nomessage))
 
-(straight-use-package 'doom-themes)
-(straight-use-package 'doom-modeline)
-(straight-use-package 'enlight)
-(straight-use-package 'magit)
-(straight-use-package 'tree-sitter)
-(straight-use-package 'tree-sitter-langs)
-(straight-use-package 'spacious-padding)
+(use-package doom-themes
+  :straight t)
+(use-package doom-modeline
+  :straight t)
+(use-package enlight
+  :straight t)
+(use-package magit
+  :straight t)
+(use-package tree-sitter
+  :straight t)
+(use-package tree-sitter-langs
+  :straight t)
+(use-package spacious-padding
+  :straight t)
+(use-package toolbox-tramp
+ :straight (toolbox-tramp
+   :type git
+   :host github
+   :repo "fejfighter/toolbox-tramp"))
+(use-package gleam-ts-mode
+  :straight t
+  :mode (rx ".gleam" eos))
+
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -30,14 +46,15 @@
  '(column-number-mode nil)
  '(custom-enabled-themes '(doom-1337))
  '(custom-safe-themes
-   '("014cb63097fc7dbda3edf53eb09802237961cbb4c9e9abd705f23b86511b0a69"
+   '("9b9d7a851a8e26f294e778e02c8df25c8a3b15170e6f9fd6965ac5f2544ef2a9"
+     "014cb63097fc7dbda3edf53eb09802237961cbb4c9e9abd705f23b86511b0a69"
      "31014fae0ca149e8bbffe40826f8f5952fdb91ea534914622d614b2219e04eaf"
      default))
  '(display-line-numbers-type 'relative)
  '(doom-modeline-bar-width 10)
  '(doom-modeline-buffer-name t)
  '(doom-modeline-check-icon nil)
- '(doom-modeline-height 64)
+ '(doom-modeline-height 32)
  '(doom-modeline-hud nil)
  '(doom-modeline-icon nil)
  '(doom-modeline-major-mode-icon nil)
@@ -51,4 +68,9 @@
  '(scroll-bar-mode nil)
  '(spacious-padding-mode t)
  '(tool-bar-mode nil))
-(custom-set-faces)
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
